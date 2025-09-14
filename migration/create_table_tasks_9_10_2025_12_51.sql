89eb60334fd8cf67793269ddfb02d658f8375039
shizakira <ikirachn@gmail.com>
2025-09-14 18:12:21 +0500

    update migration

--- migration/create_table_tasks_9_10_2025_12_51.sql
+++ migration/create_table_tasks_9_10_2025_12_51.sql
@@ -1,14 +1,23 @@
 create table if not exists users
 (
-    id          serial,
-    telegram_id bigint,
-    username    varchar(255)
+    id serial primary key
+);
+
+create table if not exists telegram_users
+(
+    id       serial primary key,
+    user_id  bigint,
+    chat_id  bigint,
+    username varchar(255),
+    foreign key (user_id) references users (id)
 );
 
 create table if not exists tasks
 (
-    id          serial,
+    id          serial primary key,
+    user_id     int,
     title       varchar(255),
     description text,
-    deadline    timestamp
+    deadline    timestamp,
+    foreign key (user_id) references users (id)
 );
